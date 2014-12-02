1179c8f419df94549c10b5bfd75c421c659526f0
Hektor Wallin <hektorw@gmail.com>
2014-12-02 15:47:32 +0100

    added comments and some functions in vec3

--- vec3.js
+++ vec3.js
@@ -8,7 +8,12 @@
 }(function() {
 
   var _arrType = typeof Float32Array !== 'undefined' ? Float32Array : Array;
+  var _sqrt = Math.sqrt;
 
+  /**
+   * @class 3 dimensional vector
+   * @name Vec3
+   */
   var Vec3 = {};
 
 
@@ -38,14 +43,14 @@
 
   Vec3.clone = function(b) {
     var a = new _arrType(3);
-    return Vec3.copy(a, b);
+    return Vec3.copy(a, b); // can be done inline instead of function call
   };
 
-  Vec3.set = function(V, x, y, z) {
-    V[0] = x;
-    V[1] = y;
-    V[2] = z;
-    return V;
+  Vec3.set = function(out, x, y, z) {
+    out[0] = x;
+    out[1] = y;
+    out[2] = z;
+    return out;
   };
 
   Vec3.scale = function(V, s) {
@@ -56,26 +61,87 @@
   };
 
 
+  /**
+   * Returns the length of a Vec3
+   * @param  {Vec3} V
+   * @return {Number}
+   */
   Vec3.length = function(V) {
-    return Math.sqrt(
-      V[0] * V[0] +
-      V[1] * V[1] +
-      V[2] * V[2]
+    var x = v[0],
+        y = v[1],
+        Z = v[2];
+    return _sqrt(
+      x * x +
+      y * y +
+      z * z
     );
   };
 
-  Vec3.normalize = function(V) {
+  
+  /**
+   * Returns the squared length of a Vec3 V
+   * @param  {Vec3} V
+   * @return {Number}
+   */
+  Vec3.lengthSquared = function(V) {
+    var x = v[0],
+        y = v[1],
+        z = v[2];
+    return x * x + y * y + z * z;
+  };
+
+
+  /**
+   * Returns the distance between two Vec3 a and b
+   * @param  {Vec3} a The first Vec3
+   * @param  {Vec3} b The second Vec3
+   * @return {Number} The distance between a and b
+   */
+  Vec3.distance = function(a, b) {
+    var x = b[0] - a[0],
+        y = b[1] - a[1],
+        z = b[2] - a[2];
+    return _sqrt(x*x + y*y + z*z);
+  };
+
+
+  /** 
+   * Returns the squared distance between two Vec3 a and b.
+   * Avoids using a sqrt calculation.
+   * 
+   * @param  {Vec3} a The first Vec3
+   * @param  {Vec3} b The second Vec3
+   * @return {Number} The squared distance between a and b
+   */
+  Vec3.distanceSquared = function(a, b) {
+    var x = b[0] - a[0],
+        y = b[1] - a[1],
+        z = b[2] - a[2];
+    return x*x + y*y + z*z;
+  };
+
+
+
+  /**
+   * Normalizes a Vec3 v and puts the result in out, settings it's length to 1
+   * 
+   * @param  {Vec3} out Vec3 which gets the result
+   * @param  {Vec3} v Vec3 to normalize
+   * @return {Vec3} return out as a normlized version of a
+   */
+  Vec3.normalize = function(out, a) {
     var l = Vec3.length(V);
-    V[0] /= l;
-    V[1] /= l;
-    V[2] /= l;
+    out[0] = V[0] / l;
+    out[1] = V[1] / l;
+    out[2] = V[2] / l;
     return V;
   };
 
-  Vec3.add = function(a, b) {
-    a[0] += b[0];
-    a[1] += b[1];
-    a[2] += b[2];
+
+  Vec3.add = function(out, a, b) {
+    out[0] = a[0] + b[0];
+    out[1] = a[1] + b[1];
+    out[2] = a[2] + b[2];
     return a;
   };
 
@@ -102,9 +168,15 @@
 
 
   Vec3.cross = function(out, a, b) {
-    out[0] = a[1] * b[2] - a[2] * b[1];
-    out[1] = a[2] * b[0] - a[0] * b[2];
-    out[2] = a[0] * b[1] - a[1] * b[0];
+    var ax = a[0],
+        ay = a[1],
+        az = a[2],
+        bx = b[0],
+        by = b[1],
+        bz = b[2];
+    out[0] = ay * bz - az * by;
+    out[1] = az * bx - ax * bz;
+    out[2] = ax * by - ay * bx;
     return out;
   };
 
@@ -115,11 +187,21 @@
   };
 
   Vec3.lerp = function(out, a, b, lerp) {
-    out[0] = a[0] + lerp * (b[0] - a[0]);
-    out[1] = a[1] + lerp * (b[1] - a[1]);
-    out[2] = a[2] + lerp * (b[2] - a[2]);
+    var ax = a[0],
+        ay = a[1],
+        az = a[2];
+    out[0] = ax + lerp * (b[0] - ax);
+    out[1] = ay + lerp * (b[1] - ay);
+    out[2] = az + lerp * (b[2] - az);
     return out;
   };
+
+
+
+  Vec3.str = function(v) {
+    return 'vec3(' + v[0] + ', ' + v[1] + ', ' + v[2] + ')';
+  };
+
 
   return Vec3;
 }));
